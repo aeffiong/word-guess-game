5b449fdb12b66511c034d33ddd110436afb57fab
aeffiong <aeffiong9@gmail.com>
2018-04-28 18:35:08 -0600

    psuedocoded game.js

---
+++ assests/javascript/game.js
@@ -0,0 +1,48 @@
+//  <!-- 2. Choose a theme for your game! exercise moves
+// 3. Use key events to listen for the letters that your players will type.
+// document.onkeyup
+
+// 4. Display the following on the page:
+
+// Press any key to get started!
+
+// Wins: (# of times user guessed the word correctly).
+
+// * If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
+
+// * As the user guesses the correct letters, reveal them: `m a d o _  _ a`.
+
+// Number of Guesses Remaining: (# of guesses remaining for the user).
+
+// Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
+
+// After the user wins/loses the game should automatically choose another word and make the user play it. --> 
+
+// first: declare variables
+    var exercises = ["squat", "pushup", "lunge", "pullup", "deadlift", "presses", "curls"];
+    console.log(exercises);
+
+// create key events (document.onkeyup)
+    document.onkeyup = function(event) {
+        var userGuess = String.fromCharCode(event.key)
+        .toLowerCase();
+
+        var userGuess = event.key;
+        console.log(userGuess);
+    }
+
+// show word on document with dashes that fill in
+
+// create conditions for them to guess the word (if, if else, else)
+
+// create the number of guesses they are allowed
+
+// show the correct letters on the document
+
+// show the incorrect letters on the document
+
+// pop up that says they won or lost
+
+// go to the next word after won or loss stated
+
+// keep tabs on page of wins and losses
