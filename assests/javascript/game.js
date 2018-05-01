8232123d873735025641dc4821e37927d421e780
aeffiong <aeffiong9@gmail.com>
2018-04-30 22:13:36 -0600

    updating

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -22,27 +22,37 @@
     var exercises = ["squat", "pushup", "lunge", "pullup", "deadlift", "presses", "curls", "burpee", "plank"];
     console.log(exercises);
 
+
+// create the number of guesses they are allowed
     var guessesLeft = 10;
+    // var numberWrong = 0;
+    // var numberRight = 0;
+    var wins = 0;
+    var losses = 0;
 
 
 // create key events (document.onkeyup)
-    document.onkeyup = function(event) {
-        var userGuess = String.fromCharCode(event.key)
-        .toLowerCase();
+    document.onclick = function(event) {
+        // var guessLetter = String.fromCharCode(event.key)
+        // .toLowerCase();
 
-        var userGuess = event.key;
-        console.log(userGuess);
+        var guessLetter = event.key;
+        console.log(guessLetter);
     }
 
 // show word on document with dashes that fill in
-    document.getElementById('letter') .innerHTML = letter;
-    document.getElementById('letter') .style.visibility = "hidden";
-    document.getElementById('underline') .style.display = "block";
-    document.getElementById('underline') .style.borderBottom = "3px solid black";
+    // document.getElementById('letter') .innerHTML = letter;
+    // document.getElementById('letter') .style.visibility = "hidden";
+    // document.getElementById('underline') .style.display = "block";
+    // document.getElementById('underline') .style.borderBottom = "3px solid black";
 
 // create conditions for them to guess the word (if, if else, else)
+    // if userGuess is correct, then show that letter in the gameSpace box, else, show that letter under "letters guessed"
+    // if userGuess guesses the word in less than the guesses, then display you win.
+    // if userGuess doesn't guess the word within the amount of guesses, then display, you lose and show the word.
+    // if userGuess incorrectly, then subtract 1 from guessesLeft. Game ends when guessesLeft = 0
+    // if userGuess guesses correctly, then show letter in game space and keep guessesLeft the same
 
-// create the number of guesses they are allowed
 
 // show the correct letters on the document
 
@@ -50,6 +60,8 @@
 
 // pop up that says they won or lost
 
-// go to the next word after won or loss stated
+// go to the next word after won or loss stated: a reset
 
 // keep tabs on page of wins and losses
+    // var html =
+    // document.getElementById()
