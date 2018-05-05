eed72221e585bba7fad04d42fb22b3e1645fc46a
aeffiong <aeffiong9@gmail.com>
2018-05-04 23:07:50 -0600

    done

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,22 +1,3 @@
-//  <!-- 2. Choose a theme for your game! exercise moves
-// 3. Use key events to listen for the letters that your players will type.
-// document.onkeyup
-
-// 4. Display the following on the page:
-
-// Press any key to get started!
-
-// Wins: (# of times user guessed the word correctly).
-
-// * If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
-
-// * As the user guesses the correct letters, reveal them: `m a d o _  _ a`.
-
-// Number of Guesses Remaining: (# of guesses remaining for the user).
-
-// Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
-
-// After the user wins/loses the game should automatically choose another word and make the user play it. --> 
 
 // first: declare variables
     var exercises = ["squat", "pushup", "lunge", "pullup", "deadlift", "presses", "curls", "burpee", "plank", "inchworm", "jump", "superman", "crunch"];
